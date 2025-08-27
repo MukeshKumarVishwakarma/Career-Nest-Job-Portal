3c4bc1bb8838f09a876d17438e129e683cebe7e8
mukesh <mukeshvishwakarma251003@gmail.com>
2025-08-27 23:27:44 +0530

    changes

--- Frontend/src/utils/constant.js
+++ Frontend/src/utils/constant.js
@@ -1,3 +1,4 @@
+export const BASE_URL = import.meta.env.VITE_API_URL || "http://localhost:9000/api";
 export const USER_API_END_POINT = "http://localhost:9000/api/v1/user"
 export const JOB_API_END_POINT = "http://localhost:9000/api/v1/job"
 export const APPLICATION_API_END_POINT="http://localhost:9000/api/v1/application";
