20af4f8aa7744fee66107923d89bade0f8347378
mukesh <mukeshvishwakarma251003@gmail.com>
2025-09-03 11:10:20 +0530

    changes

--- Backend/index.js
+++ Backend/index.js
@@ -16,11 +16,15 @@ app.use(express.json());
 app.use(express.urlencoded({extended:true}))
 app.use(cookieParser())
 
-const corsOptions = {
-    origin:'http://localhost:5173',
-    credentials:true
-}
-app.use(cors(corsOptions));
+// const corsOptions = {
+//     origin:'http://localhost:5173',
+//     credentials:true
+// }
+// app.use(cors(corsOptions));
+app.use(cors({ 
+    origin: process.env.CLIENT_URL, 
+    credentials: true,
+ }));
 
 const PORT = process.env.PORT || 1000
 
